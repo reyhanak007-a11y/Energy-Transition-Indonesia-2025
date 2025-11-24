17632affa033c5ab4c56814c7663b70b4417ce8e
reyhanak <reyhanak007@gmail.com>
2025-11-24 12:54:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -330,4 +330,5 @@ if __name__ == '__main__':
     os.makedirs('dataset', exist_ok=True)
     
     print("Server starting on http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
